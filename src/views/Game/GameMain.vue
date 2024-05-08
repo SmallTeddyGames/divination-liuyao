af46d54f3c446075c93d8cfa1f475610d2a7d620
SmallTeddy <zhangzeming1212@gmail.com>
2024-05-08 23:52:04 +0800

    docs: change readme

--- src/views/Game/GameMain.vue
+++ src/views/Game/GameMain.vue
@@ -3,9 +3,11 @@ import ResCard from './ResCard.vue'
 </script>
 
 <template>
-  <div h-full w-screen color-white p8 flex="~ col" gap-8>
-    <div h-full w-full of-hidden flex-center border="1px dashed #666" border-rd-2>
-      <ResCard />
-    </div>
+  <div style="height: calc(100vh - 48px); overflow-y: auto;" w-screen color-white p8 flex="~ col" gap-8>
+    <ResCard />
   </div>
 </template>
+
+<style scoped>
+
+</style>
--- src/views/Game/ResCard.vue
+++ src/views/Game/ResCard.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    结果卡片
+    <h3 text-2xl font-bold>卦名：{{ resInfo.name }}</h3>
+    <h3 text-xl font-bold>卦象：{{ resInfo.phase }}</h3>
+    <img :src="`${getAssetsFile(resInfo.image)}`" :alt="resInfo.name" />
+    <h3 text-xl font-bold>卦名：{{ resInfo.moral }}</h3>
   </div>
 </template>
 
 <script lang='ts' setup>
-    
+import { qianGua, duiGua, liGua, zhenGua, xunGua, kanGua, genGua, kunGua } from '../Class/GuaClass'
+import { ResultClass } from '../Class/ResClass'
+import { getAssetsFile } from '@/utils'
+
+const resGua = ref(qianGua.gua + qianGua.gua)
+const resInfo = ref(new ResultClass()[resGua.value])
+
+console.log(resInfo.value)
 </script>
+
+<style scoped>
+h3,
+img {
+  margin-bottom: 16px;
+}
+</style>
