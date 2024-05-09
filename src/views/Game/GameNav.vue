93119add6c74867309c3f4c6e512cb8ddc92ff2c
SmallTeddy <zhangzeming1212@gmail.com>
2024-05-09 10:47:55 +0800

    fix: rename

--- src/views/Game/GameNav.vue
+++ src/views/Game/GameNav.vue
@@ -4,7 +4,7 @@ import GitHubButton from './GitHubButton.vue'
 
 <template>
   <nav w-screen px4 py3 text-lg bg-transparent h-42px border="b base" color-white flex="~ gap-1 items-center">
-    <div title="Game">六爻占卜</div>
+    <div title="Game">六爻解卦</div>
     <div flex-auto></div>
     <!-- github按钮 -->
     <GitHubButton />
