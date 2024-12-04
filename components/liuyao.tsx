e29d18f2359dacfd2bd0ba404c68ceacc7bf41ca
SmallTeddy <807996041@qq.com>
2024-12-04 16:30:38 +0800

    feat: 增加说明

--- components/liuyao.tsx
+++ components/liuyao.tsx
@@ -160,18 +160,27 @@ export function Liuyao() {
 
       <Card className="w-full max-w-2xl mx-auto">
         <CardHeader>
-          <CardTitle>六爻解卦</CardTitle>
+          <CardTitle className="text-lg font-bold">六爻解卦</CardTitle>
           <CardDescription>
-            三枚硬币摇卦法说明：<br />
-            1. 一背面两字面：少阳🌱<br />
-            2. 两背面一字面：少阴🍃<br />
-            3. 三个字面：老阴🌺（变爻）<br />
-            4. 三个背面：老阳☀️（变爻）
-
+            <h3 className="text-lg font-bold">一、硬币摇卦法说明</h3>
+            <p>1. 一背面两字面：少阳🌱</p>
+            <p>2. 两背面一字面：少阴🍃</p>
+            <p>3. 三个字面：老阴🌺（变爻）</p>
+            <p>4. 三个背面：老阳☀️（变爻）</p>
             <div className="flex items-center space-x-2">
-              <img src={COIN_YANG} alt="正面" className="w-6 h-6" /> <img src={RIGHT_ALLOW} alt="右" className="w-6 h-6" /> 正面<br />
-              <img src={COIN_YIN} alt="反面" className="w-6 h-6" /> <img src={RIGHT_ALLOW} alt="右" className="w-6 h-6" /> 反面
+              <img src={COIN_YANG} alt="正面" className="w-6 h-6" /> <img src={RIGHT_ALLOW} alt="右" className="w-6 h-6" /> &nbsp;正面<br />
+              <img src={COIN_YIN} alt="反面" className="w-6 h-6" /> <img src={RIGHT_ALLOW} alt="右" className="w-6 h-6" /> &nbsp;反面
             </div>
+            <br />
+            <h3 className="text-lg font-bold">二、可测内容</h3>
+            <div>大象、运势、事业、经商、求名、婚恋、决策、时运、财运、家宅、身体、疾病、胎孕、家运、子女、周转、买卖、等人、寻人、失物、外出、考试、诉讼、求事、改行、开业</div>
+            <br />
+            <h3 className="text-lg font-bold">三、周易五不测</h3>
+            <p>1、不动不测。万物变动兆于机，事物变化之前都有先兆，能够把握就占得先机；事物未动则不需要占，如一场体育比赛，时间、地点、参加人员都还没有确定，不用预测</p>
+            <p>2、不诚不测。以现代话说，易经反映出整体论、系统论、全息论的特色，万物生命是一个整体，特别是测的人与被占者的心理有直接的关系，常言心诚则灵，念力，意念发动有力，也是信息的发射，感应自然万物接收。心不诚，念不纯，影响信息的准确性</p>
+            <p>3、不正不测。违背良知之事，非法之事，自然不应。道德二字，现在变成一个词，变成说教。其实不是，道德，大道为先，德行居后，合与道才有德，有德方合于道。所谓无道无良，无法无天，如一些腐败分子害怕东窗事发，烧香求佑，最后锒铛入狱，徒增笑柄</p>
+            <p>4、重卦不测。比如测事情，已经占得了一卦，又想再起一卦看是否相符，不能保证预测的准确性</p>
+            <p>5、没事不测。没有事情，不用测，吃饭时占一卦看吃几碗饭能饱，头脑心理都有病；抱着玩玩看的心理，也不要测，测也不准</p>
           </CardDescription>
         </CardHeader>
         <CardContent>
